b92a1fbfc2d6c379fbdb752ee26b17c133d60a49
Genesis Perez <genesissiana@Genesiss-Air.attlocal.net>
2021-07-20 20:02:45 -0700

    models files updated

--- models/User.js
+++ models/User.js
@@ -1,7 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
-
+const sequelize = require('../config/connection');
 
 // create our User model
 class User extends Model {
@@ -11,6 +10,7 @@ class User extends Model {
   }
 }
 
+// create fields/columns for User model
 User.init(
   {
     id: {
@@ -46,13 +46,12 @@ User.init(
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
         return newUserData;
       },
-      // set up beforeUpdate lifecycle "hook" functionality
+
       async beforeUpdate(updatedUserData) {
         updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
         return updatedUserData;
       }
     },
-
     sequelize,
     timestamps: false,
     freezeTableName: true,
